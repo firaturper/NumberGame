e7304f4c743929d200a04ff70168f19b62c89ceb
firaturper <firaturper88@gmail.com>
2021-06-29 14:03:13 +0300

    Update NumberGame.java

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -7,14 +7,14 @@ public class NumberGame {
 		Random rasgele = new Random();
 		Scanner scan = new Scanner(System.in);
 		while(true) {
-		System.out.println("1 ila 5 arasýnda bir sayý giriniz");
+		System.out.println("Enter A Number From 1 To 5");
 		int a = scan.nextInt();
 		int b = rasgele.nextInt(5);
 	   if(a==b) {
-		   System.out.println("Tebrikler tahmininiz Tuttu...  "+"Tahmin: "+ a + "  Çýkan sayý: "+b);
+		   System.out.println("Congratulations, Your Guess Has Been..."+"Your Guess: "+ a + "Result: "+b);
 	       continue;
 	   } else {
-		   System.out.println("Maalesef Yanlýþ Tercih...  "+"Tahmin: "+ a + "  Çýkan sayý: "+b);
+		   System.out.println("Sorry Wrong Choice..."+"Your Guess: "+ a + "Result: "+b);
 		   continue;
 	   }
 		}
